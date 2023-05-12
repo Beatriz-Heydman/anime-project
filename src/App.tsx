72594f919ff4284ad1add0371310cd2c2c6ab533
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2023-05-11 21:13:36 -0300

    feat: create CardAnime

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //Components
 import { Button } from "./components/button";
+import { CardAnime } from "./components/card-anime";
 import { Flex } from "./components/flex";
 import { Input } from "./components/input";
 import { Sidebar } from "./components/sidebar";
@@ -16,9 +17,18 @@ function App() {
       }}
     >
       <Sidebar />
-      <Flex gap="1rem" style={{ width: "100%", padding: "2rem" }}>
-        <Input />
-        <Button />
+      <Flex direction="column" style={{ width: "100%", padding: "2rem" }}>
+        <Flex gap="1rem" style={{ width: "100%", padding: "2rem" }}>
+          <Input />
+          <Button />
+        </Flex>
+        <Flex gap="1.5rem" flexWrap="wrap">
+          <CardAnime
+            animeImg="https://th.bing.com/th/id/OIP.lCW9jmu3fpwCBZsO-1xF6QHaLu?pid=ImgDet&rs=1"
+            animeName="Naruto"
+            subtitle="Episodeo 2"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
---
+++ src/components/card-anime/types.ts
@@ -0,0 +1,11 @@
+export type CardAnimeProps = {
+  dubOrSubText?: string;
+  animeName: string;
+  animeImg: string;
+  subtitle: string;
+};
+
+export type StyledCardProps = {
+  dubOrSubText?: string;
+  animeImg: string;
+};
