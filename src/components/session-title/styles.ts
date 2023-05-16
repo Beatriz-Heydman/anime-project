05c1d51d86a0b62ffc679c27bb41fe8dc2587cc7
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2023-05-16 17:18:26 -0300

    fix: changes request adjustments

--- src/components/session-title/styles.ts
+++ src/components/session-title/styles.ts
@@ -3,11 +3,14 @@ import styled from "styled-components";
 
 export const StyledSessionTitle = styled.span`
   color: #ffffff;
-  width: 100%;
+  min-width: 100%;
   border-bottom: 2px solid #444444;
   padding: 2rem 0;
-  font-family: "Inter";
   font-weight: 500;
   font-size: 1.5rem;
   display: flex;
+
+  @media (max-width: 400px) {
+    font-size: 1.1rem;
+  }
 `;
