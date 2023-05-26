9793e1e7ca7a88d3b167af0972ab239545c535b6
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2023-05-25 22:02:33 -0300

    fix: remove unecessary const

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,3 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://api.jikan.moe/v4",
 });
-
-export const testeAxios = axios.create({
-  baseURL: "https://batata.teste",
-});
